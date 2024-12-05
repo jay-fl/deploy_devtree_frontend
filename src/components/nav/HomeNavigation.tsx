6c863508bd74aecf6b36248959ba69e1bb889088
jay-fl <developerjay.fl@gmail.com>
2024-12-05 18:28:09 -0300

    first commit

---
+++ src/components/nav/HomeNavigation.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function HomeNavigation() {
+  return (
+    <>
+        <Link to="/auth/login" className="text-white p-2 uppercase font-black text-xs cursor-pointer">Iniciar Sesión</Link>
+        <Link to="/auth/register" className="bg-lime-500 text-slate-800 p-2 uppercase font-black text-xs cursor-pointer rounded-lg">Registrarme</Link>
+
+    </>
+  )
+}
